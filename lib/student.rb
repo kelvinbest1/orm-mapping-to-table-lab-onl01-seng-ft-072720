4224e793af540d515752f0f0ff298803686231a0
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-27 14:43:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
- 
+
   attr_accessor :name, :grade, :id
- 
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
   end
- 
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
- 
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
- 
+
   end
 
   def self.create(name, grade)
